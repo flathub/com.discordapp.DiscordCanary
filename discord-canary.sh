5a044098ccb8be48aa99f81e9e3f833f255d5438
guihkx <626206+guihkx@users.noreply.github.com>
2024-12-27 16:02:57 -0300

    Fix shellcheck's SC2086 issues
    $ shellcheck discord.sh
    
    In discord.sh line 9:
    socat ${SOCAT_ARGS} \
          ^-----------^ SC2086 (info): Double quote to prevent globbing and word splitting.
    
    Did you mean:
    socat "${SOCAT_ARGS}" \
    
    In discord.sh line 26:
    env TMPDIR=$XDG_CACHE_HOME zypak-wrapper /app/discord/Discord --enable-speech-dispatcher "${FLAGS[@]}" "$@"
               ^-------------^ SC2086 (info): Double quote to prevent globbing and word splitting.
    
    Did you mean:
    env TMPDIR="$XDG_CACHE_HOME" zypak-wrapper /app/discord/Discord --enable-speech-dispatcher "${FLAGS[@]}" "$@"
    
    For more information:
      https://www.shellcheck.net/wiki/SC2086 -- Double quote to prevent globbing ...
    
    Signed-off-by: McModder <me@modder.pw>

--- discord-canary.sh
+++ discord-canary.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
+read -ra SOCAT_ARGS <<<"${SOCAT_ARGS}"
+
 FLATPAK_ID=${FLATPAK_ID:-"com.discordapp.DiscordCanary"}
 OUR_SOCKET="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}/discord-ipc-0"
 DISCORD_SOCKET="${XDG_RUNTIME_DIR}/discord-ipc-0"
 rm -f "${OUR_SOCKET}"
-socat ${SOCAT_ARGS} \
+socat "${SOCAT_ARGS[@]}" \
     "UNIX-LISTEN:${OUR_SOCKET},forever,fork" \
     "UNIX-CONNECT:${DISCORD_SOCKET}" \
     &
@@ -18,5 +21,5 @@ then
 fi
 
 disable-breaking-updates.py
-env TMPDIR=$XDG_CACHE_HOME zypak-wrapper /app/discord-canary/DiscordCanary "${FLAGS[@]}" "$@"
+env TMPDIR="${XDG_CACHE_HOME}" zypak-wrapper /app/discord-canary/DiscordCanary "${FLAGS[@]}" "$@"
 kill -SIGTERM $socat_pid
